bc6499f43e18f2acd19cd803909fcbbc937c128f
JeniferLuengo <jenifersusana0505@gmail.com>
2021-02-22 21:31:59 -0300

    actualizado

--- app/src/main/java/com/example/super_hero/MailFagment.kt
+++ app/src/main/java/com/example/super_hero/MailFagment.kt
@@ -9,47 +9,52 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.super_hero.databinding.FragmentMailBinding
 
-class MailFagment: Fragment()  {
+class MailFagment: Fragment() {
 
 
+    private lateinit var binding: FragmentMailBinding
 
-
-        private lateinit var binding: FragmentMailBinding
-
-        override fun onCreateView(
+    override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
-        ): View? {
-            binding = FragmentMailBinding.inflate(inflater, container, false)
-            return binding.root
-        }
+    ): View? {
+        binding = FragmentMailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
-        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-            super.onViewCreated(view, savedInstanceState)
-            var mail="mail@mail.com"
-            binding.tvcorreo.setText(mail)
-            var asunto=""
-            binding.tvasunto.toString()
-            var descripcion=""
-            binding.tvdescripcion.toString()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        var mail = "mail@mail.com"
+        binding.tvcorreo.setText(mail)
+        var asunto = ""
+        binding.tvasunto.toString()
+        var descripcion = ""
+        binding.tvdescripcion.toString()
 
 
 
-            binding.buttonenviar.setOnClickListener{
-                sedMessage(mail,asunto,descripcion )
-            }
-        }
-
-        private fun sedMessage(mail:String,asunto:String,descripcion:String) {
-            val intent = Intent(Intent.ACTION_SEND)
-            intent.data = Uri.parse("mailto:")
-            intent.type = "text/plain"
-
-            intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(mail))
-            intent.putExtra(Intent.EXTRA_SUBJECT, asunto)
-            intent.putExtra(Intent.EXTRA_TEXT, descripcion)
+        binding.buttonenviar.setOnClickListener {
+            sedMessage(mail, asunto, descripcion)
         }
     }
+
+    private fun sedMessage(mail: String, asunto: String, descripcion: String) {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.data = Uri.parse("mailto:")
+        intent.type = "text/plain"
+
+        intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(mail))
+        intent.putExtra(Intent.EXTRA_SUBJECT, asunto)
+        intent.putExtra(Intent.EXTRA_TEXT, descripcion)
+
+        try {
+            startActivity(Intent.createChooser(intent, "elija Cliente Mail..."))
+        } catch (e:Exception){
+
+        }
+    }
+
+}
 
 
 
